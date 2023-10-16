28a854a66690c190abfbf09ef271cffbf7d9f840
Sanjeeb Padhi <sanjeebpadhi96@gmail.com>
2023-10-16 23:36:17 +0530

    Marketing Page completed

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -2,10 +2,7 @@ import Image from "next/image";
 
 export const Heroes = () => {
   return (
-    <div
-      className="flex flex-col items-center justify-center 
-                 max-w-5xl"
-    >
+    <div className="flex flex-col items-center justify-center max-w-5xl">
       <div className="flex items-center">
         <div
           className="relative w-[300px] h-[300px] sm:w-[350px]
@@ -16,6 +13,26 @@ export const Heroes = () => {
             alt="Documents"
             fill
             className="object-contain"
+          />
+          <Image
+            src="/documents-dark.png"
+            fill
+            className="object-contain hidden dark:block"
+            alt="Documents"
+          />
+        </div>
+        <div className="relative h-[400px] w-[400px] hidden md:block">
+          <Image
+            src="/reading.png"
+            fill
+            className="object-contain"
+            alt="Reading"
+          />
+          <Image
+            src="/reading-dark.png"
+            fill
+            className="object-contain hidden dark:block"
+            alt="Reading"
           />
         </div>
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -31,8 +33,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        {" "}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="Remind-theme"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
